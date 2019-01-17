8503c93c599c9bf22715988ffc8c9d71d445ebc3
LizhenZhu <zlz19961108@gmail.com>
2019-01-16 20:43:56 -0500

    something

---
+++ style.js
@@ -0,0 +1,11 @@
+var options = {
+  strings: [ 'I am an Interaction Designer ','I am an User Experience Designer', 'Imbuing empathy and perspective through experience and interaction design'],
+  typeSpeed: 60,
+  startDelay: 500,
+  backSpeed: 50,
+  backDelay: 900,
+  loop: false,
+  showCursor: false,
+  cursorChar: "|",
+}
+ityped.init('#typewords', options);
