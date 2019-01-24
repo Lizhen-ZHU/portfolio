26781b9797f0533566286fbabc57033235e083a0
Lizhen-ZHU <43251902+Lizhen-ZHU@users.noreply.github.com>
2019-01-23 23:11:56 -0500

    Update style.js

--- style.js
+++ style.js
@@ -2,7 +2,7 @@ var options = {
   strings: [ 'I am an User Experience Designer', 'I am an Interaction Designer ', 'Imbuing empathy and perspective through experience and interaction design'],
   typeSpeed: 60,
   startDelay: 500,
-  backSpeed: 50,
+  backSpeed: 70,
   backDelay: 900,
   loop: false,
   showCursor: false,
